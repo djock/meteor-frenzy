ee056b618592cc78b4c0270eb2acc4dc206fe185
Ionut Mocanu <ionutmocanu.djk@gmail.com>
2015-05-26 20:42:59 +0300

    + Work on meteor explosion

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -22,17 +22,17 @@ public class Countdown : MonoBehaviour {
 	{
 		m_CountdownTime -= 1;
 
-		Debug.Log (m_CountdownTime);
+		//Debug.Log (m_CountdownTime);
 		// 3,2,1 - GO
 		if (m_CountdownTime > 0) {
 			timeLabel.text = "" + m_CountdownTime;
-			Debug.LogError ("1");
+			//Debug.LogError ("1");
 			//Debug.Log("Seconds: " + timeRemaining);
 		} else if (m_CountdownTime == 0) {
 			timeLabel.text = "GO";
-			Debug.LogError ("2");
+			//Debug.LogError ("2");
 		} else {
-			Debug.LogError ("3");
+			//Debug.LogError ("3");
 			//UIWindow.Close();
 			//NGUITools.SetActive (countdownScreen.gameObject, false);
 			GameManager.Instance.gs = GameManager.gameState.running;
